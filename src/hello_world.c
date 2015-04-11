3050c9544d1a6190dbc1a192227d48b4a368b386
zedoul <shyeon.kim@scipi.net>
2015-04-11 12:19:15 +0200

    Added basic structure

--- src/hello_world.c
+++ src/hello_world.c
@@ -1,38 +1,58 @@
-#include <pebble.h>
+#include "hello_world.h"
 
-Window *window;
-TextLayer *text_layer;
-
-void handle_init(void) {
-	// Create a window and text layer
-	window = window_create();
-	text_layer = text_layer_create(GRect(0, 0, 144, 154));
+void init_window(const int8_t window_index) {
+    g_windows[window_index] = window_create();
 	
-	// Set the text, font, and text alignment
-	text_layer_set_text(text_layer, "Hi, I'm a Pebble!");
-	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-	
-	// Add the text layer to the window
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
-
-	// Push the window
-	window_stack_push(window, true);
-	
-	// App Logging!
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "Just pushed a window!");
+    if (WINDOW_GRAPHIC == window_index) {
+		window_set_background_color(g_windows[window_index], GColorWhite);		
+	} else if (WINDOW_MAIN == window_index) {
+	    window_set_background_color(g_windows[window_index], GColorBlack);	
+	}
 }
 
-void handle_deinit(void) {
-	// Destroy the text layer
-	text_layer_destroy(text_layer);
+int8_t show_window(const int8_t window_index, const bool animated) {
+	if (!window_stack_contains_window(g_windows[window_index])) {
+		window_stack_push(g_windows[window_index], animated);
+	} else {
+		while (window_stack_get_top_window() != g_windows[window_index]) {
+			window_stack_pop(animated);
+		}
+	}
 	
-	// Destroy the window
-	window_destroy(window);
+	return g_current_window = window_index;
+}
+			 
+void init(void) {
+	int8_t i;
+	
+	srand(time(NULL));
+	g_current_window = WINDOW_GRAPHIC;
+	
+	for (i=0; i< WINDOW_MAX; ++i) {
+		init_window(i);
+	}
+	
+	show_window(g_current_window, ANIMATED);
+	
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Initialized");
 }
 
+void deinit(void) {
+	int8_t i;
+	
+	for (i=0; i<WINDOW_MAX; ++i) {
+		window_destroy(g_windows[i]);	
+	}
+}
+
+/******************************************************************************
+   Function: main
+Description: Main function for the app.
+     Inputs: None.
+    Outputs: Number of errors encountered.
+******************************************************************************/
 int main(void) {
-	handle_init();
+	init();
 	app_event_loop();
-	handle_deinit();
+	deinit();
 }
--- src/hello_world.h
+++ src/hello_world.h
@@ -1,1 +1,44 @@
+#ifndef PEBBLE_LAYER_TEST_H_
+#define PEBBLE_LAYER_TEST_H_
 
+#include <pebble.h>
+
+/******************************************************************************
+  Enum Definitions
+******************************************************************************/
+	
+enum {
+	WINDOW_MAIN,
+	WINDOW_GRAPHIC,
+	WINDOW_MAX
+};
+
+#define STATUS_BAR_HEIGHT               16 // Applies to top and bottom status bars.
+#define SCREEN_WIDTH                    144
+#define SCREEN_HEIGHT                   168
+#define SCREEN_CENTER_POINT_X           (SCREEN_WIDTH / 2)
+#define SCREEN_CENTER_POINT_Y           (SCREEN_HEIGHT / 2 - STATUS_BAR_HEIGHT * 0.75)
+#define SCREEN_CENTER_POINT             GPoint(SCREEN_CENTER_POINT_X, SCREEN_CENTER_POINT_Y)
+#define FULL_SCREEN_FRAME               GRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT - STATUS_BAR_HEIGHT)
+#define ANIMATED                        true
+#define ANIMATED_NOT                    false
+
+/******************************************************************************
+  Structure Definitions
+******************************************************************************/
+/*	
+typedef struct someStruct {
+} __attribute__((__packed__)) struct_t;	
+*/	
+/******************************************************************************
+  Global Variables
+******************************************************************************/
+	
+Window *g_windows[WINDOW_MAX];
+uint8_t g_current_window;
+
+/******************************************************************************
+  Function Definitions
+******************************************************************************/
+
+#endif // PEBBLE_LAYER_TEST_H_
